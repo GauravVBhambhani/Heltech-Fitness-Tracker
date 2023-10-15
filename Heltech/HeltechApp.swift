f4f67ec094fa00346970e3728016f10eaabd295b
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-10-15 00:25:58 -0400

    Initial Commit

---
+++ Heltech/HeltechApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeltechApp.swift
+//  Heltech
+//
+//  Created by Gaurav Bhambhani on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HeltechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
